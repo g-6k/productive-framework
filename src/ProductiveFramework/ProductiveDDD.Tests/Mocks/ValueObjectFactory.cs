ae85a98361b332f6b9c50a37f365e43b8508ab74
gregory.cica <gregory.cica@outlook.com>
2021-05-18 19:30:05 +0200

    Add ValueObject base class and associated unit tests. close #1

---
+++ src/ProductiveFramework/ProductiveDDD.Tests/Mocks/ValueObjectFactory.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace ProductiveDDD.Tests.Mocks
+{
+    internal static class ValueObjectFactory
+    {
+        public static AddressBook HeavenAddressBook()
+        {
+            return new AddressBook("Heaven", new List<Address> { StairwayToHeaven(), Nowhere() });
+        }
+
+        public static AddressBook HellAddressBook()
+        {
+            return new AddressBook("Hell", new List<Address> { HighwayToHellAddress(), Nowhere() });
+        }
+
+        public static Address HighwayToHellAddress()
+        {
+            return new Address("Highway to hell", "Hell", 666);
+        }
+
+        public static Address Nowhere()
+        {
+            return new Address("Nowhere", "Void", 0);
+        }
+
+        public static Address StairwayToHeaven()
+        {
+            return new Address("Stairway to heaven", "Heaven", 42);
+        }
+    }
+}
